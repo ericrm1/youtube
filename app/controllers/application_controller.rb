e86f3a5ccd2c718ee11f32e32ce8339e594f0b67
Pedro Henrique Queiroz Miranda <pmiranda7410@gmail.com>
2025-04-27 16:14:13 -0300

    Fix commented methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,28 @@
 class ApplicationController < ActionController::Base
-    # helper_method :logged?
-    # helper_method :logged_channel 
-    # helper_method :show
-    # helper_method :watching_video
+    helper_method :logged?
+    helper_method :logged_channel 
+    helper_method :show
+    helper_method :watching_video
 
-    # def logged?
-    #     session[:channel_id].present?
-    # end 
+    def logged?
+        session[:channel_id].present?
+    end 
 
-    # def authenticate
-    #     unless logged? 
-    #         redirect_to login_path
-    #     end
-    # end
+    def authenticate
+        unless logged? 
+            redirect_to login_path
+        end
+    end
 
-    # def logged_channel
-    #     channel = Channel.find_by_id(session[:channel_id])
-    #     return channel
-    # end
+    def logged_channel
+        channel = Channel.find_by_id(session[:channel_id])
+        return channel
+    end
 
-    # def watching_video
-    #     @video = Video.find(params[:id])
-    #     return @video
-    # end
+    def watching_video
+        @video = Video.find(params[:id])
+        return @video
+    end
     
 
  
